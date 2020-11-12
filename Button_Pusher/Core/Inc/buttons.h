2f720c25fd3f0b2b9b10afe94431ede0ca219efa
JeroenL <jvl99@xs4all.nl>
2020-11-12 11:50:58 +0100

    First Commit

---
+++ Button_Pusher/Core/Inc/buttons.h
@@ -0,0 +1,26 @@
+/*
+ * buttons.h
+ *
+ *  Created on: 6 Oct 2020
+ *      Author: Jeroen
+ */
+
+#ifndef INC_BUTTONS_H_
+#define INC_BUTTONS_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define BUTTON_CHOOSE_METHOD_CHANCE 0
+#define BUTTON_CHOOSE_METHOD_SEQUENCE 1
+#define BUTTON_CHOOSE_METHOD_ALGORITHM 2
+
+#define BUTTON_CHOOSE_ERR -1
+
+#define BUTTON_AMOUNT 20 //TODO: Make this variable
+
+char chooseButton(char c_method);
+char signalButton(char c_button);
+extern I2C_HandleTypeDef hi2c1;
+
+#endif /* INC_BUTTONS_H_ */
